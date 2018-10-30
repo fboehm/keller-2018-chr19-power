17f38d81b1432904593b87503db07e9bfa5befa4
Frederick Boehm <frederick.boehm@gmail.com>
2018-10-30 11:38:41 -0500

    changed max_iter argument value in call to scan_pvl

--- Rscript/pvl-chr19-10mb.R
+++ Rscript/pvl-chr19-10mb.R
@@ -69,7 +69,7 @@ s_out <- scan_pvl(probs = gg2,
 #                  addcovar = cc2[ , -5], # need to remove column 5 because we have no mice from wave 5
                   covariates = cc2[ , -5], # need to remove column 5 because we have no mice from wave 5
                   start_snp = s1,
-                  n_snp = nsnp
+                  n_snp = nsnp, max_iter = 10000
 )
 
 
