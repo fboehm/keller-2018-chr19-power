310de2439602db1e37e210bf6628e3495278277b
Frederick Boehm <frederick.boehm@gmail.com>
2018-10-30 12:42:07 -0500

    created new submit and shell files

---
+++ shell_scripts/pvl-chr19-others.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# untar your R installation
+tar -xzf R-2018-09-30a.tar.gz
+tar -xzf SLIBS.tar.gz
+# make sure the script will use your R installation
+export PATH=$(pwd)/R/bin:$PATH
+export LD_LIBRARY_PATH=$(pwd)/SS:$LD_LIBRARY_PATH
+# run R, with the name of your  R script
+R CMD BATCH '--args argname='$1' run_num='$2'' Rscript/pvl-chr19-10mb-others.R 'pvl_run'$2'-'$1'.Rout'
+
+
+#rm *.Rout
