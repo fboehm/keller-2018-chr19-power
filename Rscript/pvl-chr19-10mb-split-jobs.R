ed47c57b8f6f354dfb574f57efd9e1550fcbea89
Frederick Boehm <fjboehm@wisc.edu>
2018-11-22 18:07:33 -0600

    updated 3 files for splitting scan into 25 jobs

--- Rscript/pvl-chr19-10mb-split-jobs.R
+++ Rscript/pvl-chr19-10mb-split-jobs.R
@@ -129,7 +129,7 @@ Sigma_inv <- solve(Sigma)
 # prepare table of marker indices for each call of scan_pvl
 mytab <- prep_mytab(d_size = d_size, n_snp = n_snp)
 # subset mytab based on indices for each job, where 25 jobs constitute a single 1000 by 1000 marker scan
-s_index <- ((proc_num + 1) %% 25) * 40000 # each 2d scan is broken into 25 jobs; each job is 40000 model fits, ie, 200 by 200 grid
+s_index <- ((proc_num + 1) %% 25) * 40000 # each 2d scan - 1000 by 1000 markers - is broken into 25 jobs; each job is 40000 model fits, ie, 200 by 200 grid
 mytab_sub <- mytab[(s_index + 1):(s_index + 40000), ]
 # set up parallel analysis
 list_result <- parallel::mclapply(
